f53f2b6199074b16c845619e6a1e8fb23442a817
Richard, Ribier (ESI) <Richard.RIBIER@adp.com>
2017-03-17 12:53:59 +0100

    shows how to to the Custom adapter for the todos

---
+++ app/src/main/java/com/littlecodeshop/todos/TodoAdapter.java
@@ -0,0 +1,80 @@
+package com.littlecodeshop.todos;
+
+import android.support.v7.widget.CardView;
+import android.support.v7.widget.RecyclerView;
+import android.util.Log;
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+import android.widget.ImageView;
+import android.widget.ListView;
+import android.widget.TextView;
+
+/**
+ * Created by rribier on 17/03/2017.
+ */
+
+public class TodoAdapter extends RecyclerView.Adapter<TodoAdapter.ViewHolder>{
+
+    private static final String TAG = "TodoAdapter";
+    private String[] mDataSet;
+
+    public TodoAdapter(String[] mDataSet) {
+        this.mDataSet = mDataSet;
+    }
+
+    @Override
+    public ViewHolder onCreateViewHolder(ViewGroup parent, int viewType) {
+        // create a new view
+        CardView v = (CardView) LayoutInflater.from(parent.getContext())
+                .inflate(R.layout.todo_item_card, parent, false);
+        // set the view's size, margins, paddings and layout parameters
+
+        ViewHolder vh = new ViewHolder(v);
+        return vh;
+    }
+
+
+    @Override
+    public void onBindViewHolder(ViewHolder holder, int position) {
+        holder.getTextView().setText(mDataSet[position]);
+        holder.getImageView().setImageResource(R.drawable.france_paris_eiffel_tower);
+    }
+
+    /**
+     * Returns the total number of items in the data set held by the adapter.
+     *
+     * @return The total number of items in this adapter.
+     */
+    @Override
+    public int getItemCount() {
+        return mDataSet.length;
+    }
+
+    public static class ViewHolder extends RecyclerView.ViewHolder{
+        private final TextView textView;
+        private final ImageView imageView;
+
+        public ViewHolder(View v) {
+            super(v);
+            // Define click listener for the ViewHolder's View.
+            v.setOnClickListener(new View.OnClickListener() {
+                @Override
+                public void onClick(View v) {
+                    Log.d(TAG, "Element " + getAdapterPosition() + " clicked.");
+                }
+            });
+            textView = (TextView) v.findViewById(R.id.textView);
+            imageView = (ImageView) v.findViewById(R.id.imageView);
+        }
+
+        public TextView getTextView() {
+            return textView;
+        }
+
+        public ImageView getImageView() {
+            return imageView;
+        }
+
+    }
+}
--- app/src/main/java/com/littlecodeshop/todos/TodoListActivity.java
+++ app/src/main/java/com/littlecodeshop/todos/TodoListActivity.java
@@ -4,12 +4,15 @@ import android.os.Bundle;
 import android.support.design.widget.FloatingActionButton;
 import android.support.design.widget.Snackbar;
 import android.support.v7.app.AppCompatActivity;
+import android.support.v7.widget.RecyclerView;
 import android.support.v7.widget.Toolbar;
 import android.view.View;
 import android.view.Menu;
 import android.view.MenuItem;
 
 public class TodoListActivity extends AppCompatActivity {
+
+    private RecyclerView recyclerView;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -26,6 +29,10 @@ public class TodoListActivity extends AppCompatActivity {
                         .setAction("Action", null).show();
             }
         });
+
+        String[] data = new String[]{"Hello","world","youhou"};
+        this.recyclerView = (RecyclerView) findViewById(R.id.todorecycler);
+        this.recyclerView.setAdapter(new TodoAdapter(data));
     }
 
     @Override
